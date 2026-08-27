ab2b75a241e9d8f806c3f43ee756a43a7be320d4
RLucas1102 <robenoltl@gmail.com>
2026-08-26 21:01:05 -0400

    Defined many methods; they still need testing

--- base/ImgProc.C
+++ base/ImgProc.C
@@ -10,21 +10,59 @@ ImgProc::ImgProc() :
     _img(0)
     {}
 
-ImgProc::~ImgProc() {}
+ImgProc::~ImgProc() { clear(); }
 
-void ImgProc::clear() {}
+void ImgProc::clear() {
+    if (_img != 0) {
+        delete[] _img;
+        _img = 0;
+    }
 
-void ImgProc::clear(int Nx, int Ny, int Nc) {}
+    _Nx    = 0;
+    _Ny    = 0;
+    _Nc    = 0;
+    _Nsize = 0;
+    
+}
 
-int ImgProc::GetNx() const {}
+void ImgProc::clear(int Nx, int Ny, int Nc) {
+    clear();
+    _Nx = Nx;
+    _Ny = Ny;
+    _Nc = Nc;
+    _Nsize = (long)_Nx * (long)_Ny * (long)_Nc;
+    _img = new float[_Nsize];
 
-int ImgProc::GetNy() const {}
+    #pragma omp parallel for
+    for(long i = 0; i < _Nsize; i++) {
+        _img[i] = 0;
+    }
+}
 
-int ImgProc::GetNc() const {}
+int ImgProc::GetNx() const { return _Nx; }
 
-std::vector<float> ImgProc::GetValue(int i, int j) const {}
+int ImgProc::GetNy() const { return _Ny; }
 
-void ImgProc::SetValue(int i, int j, const std::vector<float>& vals) {}
+int ImgProc::GetNc() const { return _Nc; }
+
+std::vector<float> ImgProc::GetValue(int i, int j) const {
+    std::vector<float> result(_Nc);
+
+    for (int c = 0; c < _Nc; c++) {
+        // Find channel value (c) of given pixel in contiguous memory
+        result[c] = _img[c + _Nc*(i + _Nx*j)];
+    }
+
+    return result;
+
+}
+
+void ImgProc::SetValue(int i, int j, const std::vector<float>& vals) {
+    for(int c = 0; c < _Nc; c++) {
+        // Find channel value (c) of given pixel in contiguous memory and overwrite
+        _img[c + _Nc*(i + _Nx*j)] = vals[c];
+    }
+}
 
 ImgProc::ImgProc(const ImgProc& img) {}
 
